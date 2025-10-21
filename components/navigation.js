73ac3135954e3168e155f5e647ad18caa4a68017
Nadine Becker <nadine.becker1@iu-study.org>
2025-10-21 16:06:43 +0200

    Structure code to make it more organized.

--- components/navigation.js
+++ components/navigation.js
@@ -1,13 +1,13 @@
-  $(document).ready(function () {
-    $("header").load("components/header.html", function () {
-      const currentPage = window.location.pathname.split("/").pop();
+$(document).ready(function () {
+  $("header").load("components/header.html", function () {
+    const currentPage = window.location.pathname.split("/").pop();
 
-      $(".navi a").each(function () {
-        const linkPage = $(this).attr("href");
+    $(".navi a").each(function () {
+      const linkPage = $(this).attr("href");
 
-        if (linkPage === currentPage || (linkPage === "index.html" && currentPage === "")) {
-          $(this).addClass("active");
-        }
-      });
+      if (linkPage === currentPage || (linkPage === "index.html" && currentPage === "")) {
+        $(this).addClass("active");
+      }
     });
   });
+});
